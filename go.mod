908f6d6b332d5ce4be5ed6833c78d39c4356b116
taikyunn <taichi_141@me.com>
2021-12-30 16:57:08 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module app
+
+go 1.15
+
+require (
+	github.com/gin-contrib/sessions v0.0.4
+	github.com/gin-gonic/gin v1.7.7
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
+)
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+
+	middleware "app/Middleware"
+	handler "app/handler"
+)
+
+func main() {
+	router := gin.Default()
+	// セッションCookieの設定
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+	// ログイン用のhandler
+	router.POST("/login", handler.Login)
+	// 認証済のみアクセス可能なグループ
+	authUserGroup := router.Group("/auth")
+	authUserGroup.Use(middleware.LoginCheckMiddleware())
+	{
+		// authUserGroup.GET("/getSample", handler.getSample)
+	}
+	router.Run()
+}
